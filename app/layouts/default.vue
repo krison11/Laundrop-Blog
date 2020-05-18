dec08c4782b1d99d507b213d1ecee29c41bac3f2
krison11 <54041432+krison11@users.noreply.github.com>
2020-05-18 14:40:29 +0200

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -20,9 +20,37 @@ export default class DefaultLayout extends Vue {}
 </script>
 
 <style lang="scss">
+body {
+  @apply antialiased;
+  @apply subpixel-antialiased;
+}
 
-@import 'markdown';
-@import 'main';
+.main {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-active {
+  opacity: 0;
+}
+
+.slide-left-enter,
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate(30px, 0);
+}
+
+.slide-left-leave-active,
+.slide-right-enter {
+  opacity: 0;
+  transform: translate(-30px, 0);
+}
 
 .nuxt {
   transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
