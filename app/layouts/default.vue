5aebb0cc2fed0ab40152fbd335c3565b54cf150c
krison11 <54041432+krison11@users.noreply.github.com>
2020-05-18 14:04:54 +0200

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -18,41 +18,12 @@ import SiteNav from "../components/SiteNav.vue";
 })
 export default class DefaultLayout extends Vue {}
 </script>
-@import 'markdown';
-
-body {
-  @apply antialiased;
-  @apply subpixel-antialiased;
-}
-
-.main {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-
-.page-enter-active,
-.page-leave-active {
-  transition: opacity 0.5s;
-}
-.page-enter,
-.page-leave-active {
-  opacity: 0;
-}
-
-.slide-left-enter,
-.slide-right-leave-active {
-  opacity: 0;
-  transform: translate(30px, 0);
-}
-
-.slide-left-leave-active,
-.slide-right-enter {
-  opacity: 0;
-  transform: translate(-30px, 0);
-}
 
 <style lang="scss">
+/*
+@import 'markdown';
+@import 'main';
+*/
 .nuxt {
   transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
 }
