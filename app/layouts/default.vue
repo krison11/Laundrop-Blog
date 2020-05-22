4d4b302a61d52067b13b02766ba9766a6059d41d
Krison Nilo <krisonnilo@gmail.com>
2020-05-22 18:42:15 +0200

    q

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -21,9 +21,26 @@ export default class DefaultLayout extends Vue {}
 
 <style lang="css">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200&display=swap");
-body {
-  @apply antialiased;
-  @apply subpixel-antialiased;
+
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+  font-family: "Poppins", sans-serif;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
   font-family: "Poppins", sans-serif;
 }
 
