28e8a62cc6bbe4cd1b6e99d3f9e9ba45ed6d0755
Krison Nilo <krisonnilo@gmail.com>
2020-05-22 07:45:03 +0200

    a

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,7 +13,7 @@
               :data-src="post.image || 'https://source.unsplash.com/random/640x340'"
               v-lazy-load
             />
-            <!-- section textile -->
+            <!-- section text -->
             <section class="p-6 bg-white mx-auto">
               <!-- title -->
               <h2 class="text-2xl mb-2">{{ post.title }}</h2>
